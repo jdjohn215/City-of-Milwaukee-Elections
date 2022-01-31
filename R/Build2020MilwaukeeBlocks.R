9ec8a758ac3e1e831b3c0097e4fea7039917e6f6
jdjohn215 <jdjohn215@gmail.com>
2022-01-31 12:21:01 -0600

    add political boundaries

--- R/Build2020MilwaukeeBlocks.R
+++ R/Build2020MilwaukeeBlocks.R
@@ -19,5 +19,18 @@ mke.blocks <- pl.blocks %>%
   filter(COUSUB == "53000") %>%
   pl_select_standard() %>%
   inner_join(mke.vtd) %>%
-  select(GEOID, vtd, ward, starts_with("pop"), starts_with("vap"))
+  inner_join(pl.blocks %>% select(GEOID, SLDL = SLDL18, SLDU = SLDU18)) %>%
+  select(GEOID, SLDL, SLDU, vtd, ward, starts_with("pop"), starts_with("vap"))
+
+# add aldermanic districts
+aldermanic.2012 <- sf::st_read("boundaries/CityofMilwaukeeWards2012.geojson") %>%
+  sf::st_set_geometry(NULL) %>%
+  mutate(ward_2012 = str_pad(ward_2012, width = 4, side = "left", pad = "0"))
+
+mke.blocks <- mke.blocks <- mke.blocks %>%
+  inner_join(aldermanic.2012, by = c("ward" = "ward_2012")) %>%
+  mutate(SLDL = as.numeric(SLDL),
+         SLDU = as.numeric(SLDU)) %>%
+  select(GEOID, SLDL, SLDU, vtd, aldermanic_2012, ward, everything())
+
 write_csv(mke.blocks, "census-data/MilwaukeeBlocks2020With2012WardAssignments.csv")
