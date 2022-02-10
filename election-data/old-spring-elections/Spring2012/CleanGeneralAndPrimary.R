7682b9aae027abd3c7a51059eefb178b9d9c3425
jdjohn215 <jdjohn215@gmail.com>
2022-02-10 17:54:34 -0600

    update

--- election-data/old-spring-elections/Spring2012/CleanGeneralAndPrimary.R
+++ election-data/old-spring-elections/Spring2012/CleanGeneralAndPrimary.R
@@ -8,7 +8,7 @@ primary <- readxl::read_excel("election-data/old-spring-elections/Spring2012/Pri
   mutate(ward = word(string, 1),
          votes = word(string, -4, -1)) %>%
   select(ward, votes) %>%
-  separate(votes, into = c("tom_barret", "ieshuh_griffin", "edward_c_mcdonald", "write_in"),
+  separate(votes, into = c("tom_barrett", "ieshuh_griffin", "edward_c_mcdonald", "write_in"),
            sep = " ", convert = T)
 primary %>% summarise(across(where(is.numeric), sum)) # matches
 write_csv(primary, "election-data/old-spring-elections/mayor-city-of-milwaukee_2012primary.csv")
@@ -19,7 +19,7 @@ general <- readxl::read_excel("election-data/old-spring-elections/Spring2012/Pri
   mutate(ward = word(string, 1),
          votes = word(string, -3, -1)) %>%
   select(ward, votes) %>%
-  separate(votes, into = c("tom_barret", "edward_c_mcdonald", "write_in"),
+  separate(votes, into = c("tom_barrett", "edward_c_mcdonald", "write_in"),
            sep = " ", convert = T)
 general %>% summarise(across(where(is.numeric), sum)) # matches
 write_csv(general, "election-data/old-spring-elections/mayor-city-of-milwaukee_2012general.csv")
