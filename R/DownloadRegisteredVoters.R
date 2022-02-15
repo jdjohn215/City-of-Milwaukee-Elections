bbe509d9857b63a5642dfdf0f8b52696159e8ba2
jdjohn215 <jdjohn215@gmail.com>
2022-02-15 09:37:07 -0600

    add reg voters 2022/02/01

--- R/DownloadRegisteredVoters.R
+++ R/DownloadRegisteredVoters.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 # Download the registered voters file
 tmpfile <- tempfile()
-download.file("https://elections.wi.gov/sites/elections/files/2022-01/RegisteredVotersByWard_01-01-2022.xlsx",
+download.file("https://elections.wi.gov/sites/elections/files/2022-02/RegisteredVotersByWard_02-01-2022.xlsx",
               tmpfile)
 wi.regvoters <- readxl::read_excel(tmpfile)
 unlink(tmpfile)
@@ -13,7 +13,7 @@ mke.regvoters <- wi.regvoters %>%
   filter(str_detect(Muni, "CITY OF MILWAUKEE")) %>%
   mutate(ward = word(ward, -1)) %>%
   select(ward, reg_voters = `Voter Count`) %>%
-  mutate(date = "2022-01-01",
+  mutate(date = "2022-02-01",
          ward = as.numeric(ward)) %>%
   arrange(ward)
-write_csv(mke.regvoters, "reg-voters/CityOfMilwaukee_RegisteredVoters_01-01-2022.csv")
+write_csv(mke.regvoters, "reg-voters/CityOfMilwaukee_RegisteredVoters_02-01-2022.csv")
